06df0fec5edc2c1024b144427cd68d01d276f029
Fer <fer_namtz@hotmail.com>
2024-08-19 20:22:34 -0300

    MusicPlayerCommit

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { SectionBox } from "./components/Modules";
+import { SectionSongs } from "./components/Modules";
+import { SectionCircle } from "./components/Modules";
+import {user, arrayListenAgain, arrayArtist, arrayQuickP,arrayRecAlbum} from "./components/Api";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <img className="logo" src="./src/assets/Header/Frame 5.png"></img>
+        <input className="buscador" type="text" placeholder="Search"></input>
+        <img className="share" src="./src/assets/Header/cast.png"></img>
+        <img className="avatar" src="./src/assets/Header/Avatar.png"></img>
+      </header>
+      <main>
+        <SectionBox
+          user={user}
+          texto1={user.name}
+          texto2="Listen Again"
+          arrayCard={arrayListenAgain}
+          />
+        <SectionCircle
+          user={user}
+          texto1="SIMILAR TO"
+          texto2="[Artista]"
+          arrayCard={arrayArtist}
+          />
+        <SectionSongs
+          user={user}
+          texto1="START RADIO FROM A SONG"
+          texto2="Quick picks"
+          arrayCard={arrayQuickP}
+          />
+        <SectionBox
+          user={user}
+          texto1=""
+          texto2="Recommended albums"
+          arrayCard={arrayRecAlbum}
+        />
+      </main>
+
+      <footer className="reproductor">
+        <div className='BotonesIzqGrupo' >
+        <img className="BotonesIzq" src="/src/assets/Footer/skip_previous.png" alt="" />
+        <img className="BotonesIzq" src="/src/assets/Footer/play_arrow.png" alt="" />
+        <img className="BotonesIzq" src="/src/assets/Footer/skip_next.png" alt="" />
+
+        </div>
+        <div className="cancion">
+          <img src="/src/assets/Body/slipknot.jpg" alt="" />
+          <div className="text">
+            <h4>Slipknot</h4>
+            <p>Slipknot - 100K views - 12562 likes</p>
+          </div>
+          
+          <img className ="botonesMid" src="/src/assets/Footer/thumb_down.png" alt="" /> 
+          <img className ="botonesMid" src="/src/assets/Footer/thumb_up.png" alt="" /> 
+          <img className ="botonesMid" src="/src/assets/Footer/more_vert.png" alt="" /> 
+          
+        </div>
+        <div className='BotonesDerGrupo'>
+        <img className="BotonesDer" src="/src/assets/Footer/repeat.png" alt="" />
+        <img className="BotonesDer" src="/src/assets/Footer/volume_up.png" alt="" />
+        <img className="BotonesDer" src="/src/assets/Footer/arrow_drop_down.png" alt="" />
+        </div>
+        
+      </footer>
     </>
   )
 }
---
+++ src/components/Api.tsx
@@ -0,0 +1,127 @@
+type playlist = {
+    img: string;
+    texto1: string;
+    texto2: string;
+    songs?: string;
+  };
+export const user = {
+    name: "Fernando Namtz",
+    img: "/src/assets/Header/Avatar.png",
+  };
+  export const arrayListenAgain: playlist[] = [
+    {
+      img: "/src/assets/Body/Rectangle 1.png",
+      texto1: "PlayList Name",
+      texto2: "Artist Name",
+      songs: "20 songs",
+    },
+    {
+      img: "/src/assets/Body/Rectangle 1.png",
+      texto1: "PlayList 2",
+      texto2: "Artist Name",
+      songs: "20 songs",
+    },
+    {
+      img: "/src/assets/Body/Rectangle 1.png",
+      texto1: "PlayList 2",
+      texto2: "Artist Name",
+      songs: "20 songs",
+    },
+    {
+      img: "/src/assets/Body/Rectangle 1.png",
+      texto1: "PlayList 2",
+      texto2: "Artist Name",
+      songs: "20 songs",
+    },
+
+  ];
+  export const arrayArtist: playlist[] = [
+    {
+      img: "/src/assets/Body/Round 1.png",
+      texto1: "Listen Again",
+      texto2: "4000 suscribers",
+    },
+    {
+      img: "/src/assets/Body/Round 1.png",
+      texto1: "Listen Again",
+      texto2: "4000 suscribers",
+    },
+    {
+      img: "/src/assets/Body/Round 1.png",
+      texto1: "Listen Again",
+      texto2: "4000 suscribers",
+    },
+    {
+      img: "/src/assets/Body/Round 1.png",
+      texto1: "Listen Again",
+      texto2: "4000 suscribers",
+    },
+
+    
+  ];
+export const arrayQuickP: playlist[] = [
+    {
+      img: "/src/assets/Body/slipknot.jpg",
+      texto1: "Cancion",
+      texto2: "Artista",
+    },
+    {
+      img: "/src/assets/Body/slipknot.jpg",
+      texto1: "Cancion",
+      texto2: "Artista",
+    },
+    {
+      img: "/src/assets/Body/slipknot.jpg",
+      texto1: "Cancion",
+      texto2: "Artista",
+    },
+    {
+      img: "/src/assets/Body/slipknot.jpg",
+      texto1: "Cancion",
+      texto2: "Artista",
+    },
+    {
+      img: "/src/assets/Body/slipknot.jpg",
+      texto1: "Cancion",
+      texto2: "Artista",
+    },
+    {
+      img: "/src/assets/Body/slipknot.jpg",
+      texto1: "Cancion",
+      texto2: "Artista",
+    },
+    {
+      img: "/src/assets/Body/slipknot.jpg",
+      texto1: "Cancion",
+      texto2: "Artista",
+    },
+    {
+      img: "/src/assets/Body/slipknot.jpg",
+      texto1: "Cancion",
+      texto2: "Artista",
+    },
+    
+  ];
+export const arrayRecAlbum: playlist[] = [
+    {
+      img: "/src/assets/Body/Rectangle 2.png",
+      texto1: "Listen Again",
+      texto2: "Listen Again",
+    },
+    {
+      img: "/src/assets/Body/Rectangle 2.png",
+      texto1: "Listen Again",
+      texto2: "Listen Again",
+    },
+    {
+      img: "/src/assets/Body/Rectangle 2.png",
+      texto1: "Listen Again",
+      texto2: "Listen Again",
+    },
+    {
+      img: "/src/assets/Body/Rectangle 2.png",
+      texto1: "Listen Again",
+      texto2: "Listen Again",
+    },
+    
+];
